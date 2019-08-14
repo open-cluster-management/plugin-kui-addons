1c4dba8a3e98f63a4effc105445527783ca31483
Hanqiu Zhang <Hanqiu.Zhang@ibm.com>
2019-08-14 11:43:01 -0400

    Fix dep core (#7)
    * fix: set core to dev-dependencies
    
    * fix: add command blocking logics

--- src/plugin.ts
+++ src/plugin.ts
@@ -16,8 +16,27 @@
 
 import { CommandRegistrar } from '@kui-shell/core/models/command'
 
+const bashLikeRoutes = ['/git/status','/git/diff','/export','/cd'];
+const k8sRoutes = ['/istio/install','/istio/uninstall','/istio/ingress','/istio/status',
+'/bookinfo/install','/bookinfo/uninstall','/bookinfo/create',
+'/kiali/install','/kiali/delete','/kiali/console', '/kiali/graph',
+'/k8s/kedit','/k8s/kdebug'];
+//const kuiRoutes = ['']
+
+const blockKUICommand = async (route: string,commandTree: CommandRegistrar)=>{
+  // using listen->find->listen to block
+  commandTree.listen(route,() => {
+    return Promise.reject('Command is disabled')
+  },{noAuthOk: true,inBrowserOk: true}) 
+
+  await commandTree.find(route)
+
+  commandTree.listen(route,() => {
+    return Promise.reject('Command is disabled')
+  },{noAuthOk: true,inBrowserOk: true})
+}
+
 export default async (commandTree: CommandRegistrar) => {
-  return await commandTree.listen('/foo', () => new Promise((resolve,reject) => {
-      reject('Command not found')
-    }), { noAuthOk: true, inBrowserOk: false })
+  const allRoutes =  [...bashLikeRoutes,...k8sRoutes]
+  return Promise.all(allRoutes.map((route)=>blockKUICommand(route,commandTree)))
 }
