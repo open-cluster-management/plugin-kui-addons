b8889a51988a060c972ca5b7d89d47c3efd99df5
Hanqiu Zhang <Hanqiu.Zhang@ibm.com>
2019-08-22 17:51:55 -0400

    fix: move help to preload
    fix: move help to preload

--- src/plugin.ts
+++ src/plugin.ts
@@ -15,7 +15,6 @@
  */
 
 import { CommandRegistrar } from '@kui-shell/core/models/command'
-import { repl } from '@kui-shell/core/core/repl'
 
 const bashLikeRoutes = ['/git/status','/git/diff'];
 const k8sRoutes = ['/istio/install','/istio/uninstall','/istio/ingress','/istio/status',
@@ -31,7 +30,6 @@ const blockKUICommand = async (route: string,commandTree: CommandRegistrar)=>{
   commandTree.listen(route,() => {
     return Promise.reject('Command is disabled')
   },{noAuthOk: true,inBrowserOk: true}) 
-
   await commandTree.find(route)
 
   commandTree.listen(route,() => {
@@ -39,19 +37,7 @@ const blockKUICommand = async (route: string,commandTree: CommandRegistrar)=>{
   },{noAuthOk: true,inBrowserOk: true})
 }
 
-const redirectHelp = async (commandTree: CommandRegistrar)=> {
-  commandTree.listen('help',() => {
-    return Promise.reject('Command is disabled')
-  },{noAuthOk: true,inBrowserOk: true}) 
-
-  await commandTree.find('help')
-
-  commandTree.listen('help', async () => {
-    return await repl.qexec('getting started')
-  },{noAuthOk: true,inBrowserOk: true})
-}
-
 export default async (commandTree: CommandRegistrar) => {
   const allRoutes =  [...bashLikeRoutes,...k8sRoutes,...coreSupportRoutes]
-  return Promise.all([...allRoutes.map((route)=>blockKUICommand(route,commandTree)), redirectHelp(commandTree)])
+  return Promise.all([...allRoutes.map((route)=>blockKUICommand(route,commandTree))])
 }
