91db1ff757ebf17f6e9f869bdbaca90013880b35
Hanqiu Zhang <Hanqiu.Zhang@ibm.com>
2019-09-17 16:24:05 -0400

    fix: force ! to always go through rbash (#15)
    same as the `ls` solution, to always route ! to normal websocket bash-like path

--- src/plugin.ts
+++ src/plugin.ts
@@ -47,7 +47,20 @@ const rewriteLSCommand = async (commandTree: CommandRegistrar)=>{
   },{noAuthOk: true,inBrowserOk: true})
 }
 
+const rewriteExecCommand = async (commandTree: CommandRegistrar)=>{
+  // using listen->find->listen to block
+  const route='/!'
+  commandTree.listen(route,() => {
+    return Promise.reject('Command is disabled')
+  },{noAuthOk: true,inBrowserOk: true}) 
+  await commandTree.find(route)
+  commandTree.listen(route,(opts: EvaluatorArgs) => {
+    debug('! dispatch to shell')
+    return dispatchToShell(opts)
+  },{noAuthOk: true,inBrowserOk: true})
+}
+
 export default async (commandTree: CommandRegistrar) => {
   const allRoutes =  [...bashLikeRoutes,...k8sRoutes,...coreSupportRoutes]
-  return Promise.all([rewriteLSCommand(commandTree),rewriteLSCommand(commandTree),...allRoutes.map((route)=>blockKUICommand(route,commandTree))])
+  return Promise.all([rewriteExecCommand(commandTree),rewriteLSCommand(commandTree),rewriteLSCommand(commandTree),...allRoutes.map((route)=>blockKUICommand(route,commandTree))])
 }
